f644b5947f63803f46ae6ee6391d27fd968a6f37
petrikoj <janpetrikowski@protonmail.com>
2022-09-09 10:47:55 +0200

    Working on Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image, VStack, Text, Box, HStack } from "@chakra-ui/react";
+import { AiFillGithub } from "react-icons/ai";
+import { RiCopyrightFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
@@ -12,8 +14,38 @@ const Footer = () => {
       p={"2.5"}
       pos={"sticky"}
       bgGradient={"linear(to-tl, pink.200, red.300)"}
+      align={"center"}
     >
-      This is my footer
+      <Box>
+        <VStack>
+          <Text textAlign={"center"} fontFamily={"mono"} fontSize={"medium"}>
+            Data provided by:
+          </Text>
+          <a href={"https://www.themoviedb.org/"} target={"_blank"}>
+            <Image
+              w={"40"}
+              h={"auto"}
+              src={
+                "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
+              }
+              alt={"The Movie Database Logo"}
+            />
+          </a>
+        </VStack>
+      </Box>
+      <Box>
+        <HStack>
+          <RiCopyrightFill size={"24"} />
+          <Text fontFamily={"mono"} fontSize={"large"}>
+            petrikoj 2022
+          </Text>
+        </HStack>
+      </Box>
+      <Box>
+        <a href={"https://github.com/petrikoj"} target={"_blank"}>
+          <AiFillGithub size={"44"} />
+        </a>
+      </Box>
     </Flex>
   );
 };

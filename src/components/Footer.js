090350e7b9d41d2baee281e2cf65a8ffbcd83de4
petrikoj <janpetrikowski@protonmail.com>
2022-09-14 20:31:47 +0200

    Improved responsiveness of Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,47 +6,39 @@ import { RiCopyrightFill } from "react-icons/ri";
 const Footer = () => {
   return (
     <Flex
+      direction={["column-reverse", "row"]}
       as={"footer"}
-      h={"44"}
+      h={["sm", "40", "60"]}
       w={"100%"}
       mb={"0"}
       mt={"20"}
-      p={"2.5"}
+      p={"10"}
       pos={"sticky"}
       // bgGradient={"linear(to-tl, pink.200, red.300)"}
       bgGradient={"linear(to-tl, yellow.100, cyan.100, red.200)"}
       align={"center"}
+      justify={"center"}
     >
-      <Box>
-        <VStack>
-          <Text textAlign={"center"} fontFamily={"mono"} fontSize={"medium"}>
-            Data provided by:
-          </Text>
-          <a
-            href={"https://www.themoviedb.org/"}
-            target={"_blank"}
-            rel={"noreferrer"}
-          >
-            <Image
-              w={"40"}
-              h={"auto"}
-              src={
-                "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
-              }
-              alt={"The Movie Database Logo"}
-            />
-          </a>
-        </VStack>
-      </Box>
-      <Box>
-        <HStack>
-          <RiCopyrightFill size={"24"} />
-          <Text fontFamily={"mono"} fontSize={"large"}>
-            petrikoj 2022
-          </Text>
-        </HStack>
-      </Box>
-      <Box>
+      <VStack m={"4"}>
+        <Text textAlign={"center"} fontFamily={"mono"} fontSize={"medium"}>
+          Data provided by:
+        </Text>
+        <a
+          href={"https://www.themoviedb.org/"}
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          <Image
+            w={"40"}
+            h={"auto"}
+            src={
+              "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
+            }
+            alt={"The Movie Database Logo"}
+          />
+        </a>
+      </VStack>
+      <Box m={"4"}>
         <a
           href={"https://github.com/petrikoj"}
           target={"_blank"}
@@ -55,6 +47,12 @@ const Footer = () => {
           <AiFillGithub size={"44"} />
         </a>
       </Box>
+      <HStack m={"4"}>
+        <RiCopyrightFill size={"24"} />
+        <Text fontFamily={"mono"} fontSize={"large"}>
+          petrikoj 2022
+        </Text>
+      </HStack>
     </Flex>
   );
 };
